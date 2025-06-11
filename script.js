570cee229fe7080090b64332a6c2d104820756df
OneTopUPZone <ikhwanbusiness1@gmail.com>
2025-06-11 14:17:22 +0700

    Update script.js

--- script.js
+++ script.js
@@ -20,3 +20,27 @@ document.getElementById("topupForm").addEventListener("submit", function (e) {
   document.getElementById("successMsg").classList.remove("hidden");
   this.reset(); // Kosongkan form
 });
+document.getElementById("topupForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const game = document.getElementById("gameSelect").value;
+  const diamond = document.getElementById("diamondSelect").value;
+  const userid = document.querySelector('input[placeholder="Masukkan User ID"]').value;
+  const email = document.querySelector('input[placeholder="Masukan Alamat Email Anda"]').value;
+  const payment = document.querySelector('select').value;
+
+  fetch("https://script.google.com/macros/s/AKfycbyEQFBp2WFoXzj3Pwul44yz7He2SXJdO-hpP7N6iHM3zFZhJkkMxYxk72rgOXMO7Ag/exec", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ game, diamond, userid, email, payment })
+  })
+  .then(res => res.text())
+  .then(data => {
+    document.getElementById("successMsg").classList.remove("hidden");
+    document.getElementById("topupForm").reset();
+  })
+  .catch(err => console.error("Error:", err));
+});
+
